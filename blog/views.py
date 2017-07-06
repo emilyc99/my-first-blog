32933cce3f9402b6da26b1c1a59e3bc8ddd43f79
Emily Chang <Emily Chang>
2017-07-06 14:03:46 -0500

    making my blog pretty

--- blog/views.py
+++ blog/views.py
@@ -37,3 +37,6 @@ def post_edit(request, pk):
 	else:
 		form = PostForm(instance-post)
 	return render(request, 'blog/post_edit.html', {'form':form})
+
+def home(request):
+	return render(request, 'blog/index.html')
